0baee3da4506bee3a17816cf329fa12512e0e8f2
DylanGuedes <djmgguedes@gmail.com>
2015-02-20 11:59:32 -0200

    stable progress bars

--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,10 @@ ActiveRecord::Schema.define(version: 20150220124321) do
     t.integer  "kills",      default: 0
     t.integer  "gold",       default: 100
     t.integer  "exp",        default: 0
-    t.float    "hp_max",     default: 20.0
-    t.float    "hp_min",     default: 20.0
+    t.float    "hp_max",     default: 100.0
+    t.float    "hp_min",     default: 100.0
+    t.float    "mp_max",     default: 20.0
+    t.float    "mp_min",     default: 20.0
     t.float    "damage",     default: 15.0
     t.boolean  "atacable",   default: false
     t.float    "defense",    default: 0.0
