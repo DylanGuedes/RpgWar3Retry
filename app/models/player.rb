33b111af3bcdaef4606cfb646bfe749487f2a290
DylanGuedes <djmgguedes@gmail.com>
2015-02-20 12:03:26 -0200

    stable percentage methods

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,17 @@
 class Player < ActiveRecord::Base
+  include ApplicationHelper
   belongs_to :user
   has_one :role
 
   def playable?
     !self.role.nil?
+  end
+
+  def hp_in_percentage
+    in_percentage self.hp_max, self.hp_min
+  end
+
+  def mp_in_percentage
+    in_percentage self.mp_max, self.mp_min
   end
 end
