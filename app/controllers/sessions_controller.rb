d41cc739dd33b2fb3620d33402ab975f11a976c7
DylanGuedes <djmgguedes@gmail.com>
2015-02-20 10:16:54 -0200

    new sessionshelper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
-    # user = User.find_by(login: params[:session][:login].downcase)
-    # if user && user.authenticate(params[:session][:password])
-    #   sign_in ?user
+    user = User.find_by(login: params[:session][:login].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
     #   params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-    #   redirect_to user
-    # else
-    #   flash.now[:danger] = 'Invalid email/password combination'
-    #   render 'new'
-    # end
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
